d4ba4b387164d51ec6b2cdc28a2f568d0fd4d8c1
spe_ty <62170193+Tangle0307@users.noreply.github.com>
2024-05-01 17:50:14 +0900

    update supabase db call

--- app/images/page.tsx
+++ app/images/page.tsx
@@ -1,7 +1,7 @@
 import { supabase } from '@/ui/utility/supabase/supabase'
 
 export default async function Page() {
-    const { data, error } = await supabase.from('labeled_image').select('*')
+    const { data, error } = await supabase().from('labeled_image').select('*')
 
     console.log(data)
 
--- ui/utility/supabase/supabase.ts
+++ ui/utility/supabase/supabase.ts
@@ -1,6 +1,38 @@
+'use server'
+
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-import { createClient } from './server'
+export const supabase = () => {
+    const cookieStore = cookies()
 
-const cookieStore = cookies()
-export const supabase = createClient(cookieStore)
+    return createServerClient(
+        'https://lxziyihbxcoatqaarfur.supabase.co',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4eml5aWhieGNvYXRxYWFyZnVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQyODAwNDAsImV4cCI6MjAyOTg1NjA0MH0.I2ZbK3F-c3kxaRczyAx7zVv1xv3YXLJk-nr5CY2W3j4',
+        {
+            cookies: {
+                get(name: string) {
+                    return cookieStore.get(name)?.value
+                },
+                set(name: string, value: string, options: CookieOptions) {
+                    try {
+                        cookieStore.set({ name, value, ...options })
+                    } catch (error) {
+                        // The `set` method was called from a Server Component.
+                        // This can be ignored if you have middleware refreshing
+                        // user sessions.
+                    }
+                },
+                remove(name: string, options: CookieOptions) {
+                    try {
+                        cookieStore.set({ name, value: '', ...options })
+                    } catch (error) {
+                        // The `delete` method was called from a Server Component.
+                        // This can be ignored if you have middleware refreshing
+                        // user sessions.
+                    }
+                },
+            },
+        },
+    )
+}
